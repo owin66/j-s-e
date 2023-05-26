3ce734c89d0ad4f44b46bcc88479d18c3d446416
owin66 <dmitriysakaluk@gmail.com>
2023-05-26 18:37:24 +0300

    finish_2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  let i = 1;
+  let string = "";
+  while (i <= num) {
+    string += str;
+    i++;
+  }
+  return string;
 };
 
 // Do not edit below this line
