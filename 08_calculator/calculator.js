9b50a6928e7aa3c149bdf3af7ffc047fdd543132
owin66 <dmitriysakaluk@gmail.com>
2023-05-27 08:07:55 +0300

    finish_11

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,20 @@
-const add = function() {
-	
+const add = function () {};
+
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const subtract = function() {
-	
+const sum = function (a, b) {
+  return a + b;
 };
 
-const sum = function() {
-	
+const multiply = function () {};
+
+const power = function (a, b) {
+  a * b;
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = function () {};
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +23,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,5 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (a) {
+  return a.map((b, c) => b.title);
 };
 
 // Do not edit below this line
