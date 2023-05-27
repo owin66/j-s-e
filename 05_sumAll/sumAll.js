2db77e202cf37d1cdc6d43e93caad472e07f6c33
owin66 <dmitriysakaluk@gmail.com>
2023-05-27 07:50:20 +0300

    finish_5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,7 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  let min = Math.min(a, b),
+    max = Math.max(a, b);
+  return ((max - min + 1) * (min + max)) / 2;
 };
 
 // Do not edit below this line
